021ec0834387fa40cf863eaa9c768897f06e812e
Jae Lee <jaequery@gmail.com>
2011-11-22 09:10:07 -0800

    added capture php file

---
+++ capture.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+* usage) http://localhost/capture.php?url=http://www.google.com
+*/
+
+// lib path
+$xvfb_path = '/usr/bin';
+$cutycapt_path = "/home/user/scripts/cutycapt/CutyCapt";
+
+// capture settings
+$screen = '1280x1024x24';
+$output_path = '/home/user/www/images/capture.png';
+$url = $_GET['url']; // set it however you want
+//$url = 'http://www.yahoo.com'; // set it manually
+
+// run it
+exec($xvfb_path . '/xvfb-run --server-args="-screen 0, '.$resolution.'" '.$cutycapt_path.'/CutyCapt --url="'.$url.'" --out="'.$output_path);
+
+?>
+
